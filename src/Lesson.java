46ce3708796a5bc1aa2ee44150d2c1150ea92d27
Mukhammed Dev <muhammedtoichubai@gmail.com>
2024-07-24 20:24:36 +0600

    Initial commit

---
+++ src/Lesson.java
@@ -0,0 +1,12 @@
+public class Lesson {
+    public static void main(String[] args) {
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       char[] symbol = {'K', 'a', 'i', 'r', 'a', 't'} ;
+
+        String phoneNumber = "+789700600500";
+
+        System.out.println(phoneNumber.startsWith("+996"));
+
+
+    }
+
+
+
+}
